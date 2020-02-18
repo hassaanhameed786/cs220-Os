5e92c263e5d22cbdd92431ac7455586dd71eab9b
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2020-02-19 03:10:04 +0500

    man fork in terminal

--- fork.c
+++ fork.c
@@ -26,3 +26,18 @@ int int main(void) {
 	return 0;
     
 }
+
+/* 
+
+
+fork ();   // Line 1
+fork ();   // Line 2
+fork ();   // Line 3
+
+       L1       // There will be 1 child process 
+    /     \     // created by line 1.
+  L2      L2    // There will be 2 child processes
+ /  \    /  \   //  created by line 2
+L3  L3  L3  L3  // There will be 4 child processes 
+                // created by line 3
+		*/
